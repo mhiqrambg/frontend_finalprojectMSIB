f359e025e6dafbfda248deada0fe049a9b8d77e7
mhiqrambg <muhiqrambahring@gmail.com>
2024-06-15 02:16:20 +0800

    first commit

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:snippet_coder_utils/ProgressHUD.dart';
+import 'package:snippet_coder_utils/hex_color.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool isAPIcallProcces = false;
+  bool hidePassword = true;
+  GlobalKey<FormState> globalFormKey = GlobalKey<FormState>();
+  String? username;
+  String? password;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: HexColor('#e0f0ea'),
+        body: ProgressHUD(
+          child: Form(
+            key: globalFormKey,
+            child: _loginUI(context),
+          ),
+          inAsyncCall: isAPIcallProcces,
+          opacity: 0.3,
+          key: UniqueKey(),
+        ),
+      ),
+    );
+  }
+
+  Widget _loginUI(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.width,
+            decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                  Color(0xff95adbe),
+                  Color(0xfe0f0ea),
+                ])),
+          )
+        ],
+      ),
+    );
+  }
+}
